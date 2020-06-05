e84ecc8e30433157c675bf564f18a782b6620c2e
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-06-05 16:33:20 -0400

    added artsdata API

---
+++ app/services/artsdata_api/v1/client.rb
@@ -0,0 +1,61 @@
+module ArtsdataAPI
+  module V1
+    # Main Client for Artsdata.ca
+    class Client
+      API_ENDPOINT = 'http://db.artsdata.ca'.freeze
+
+      attr_reader :oauth_token
+
+      def initialize(oauth_token: nil, graph_repository: 'artsdata')
+        @oauth_token = oauth_token
+        @graph_repository = graph_repository
+      end
+
+      def execute_sparql sparql 
+        resp = request_json(
+          http_method: :post,
+          endpoint: "/repositories/#{@graph_repository}",
+          params: { 'query': sparql.squish }
+        )
+        return resp['results']['bindings']
+      end
+
+      private
+
+      def client
+        @_client ||= Faraday.new(API_ENDPOINT) do |client|
+          client.request :url_encoded
+          client.adapter Faraday.default_adapter
+          client.headers['Authorization'] = "token #{oauth_token}" if oauth_token.present?
+        end
+      end
+
+
+      def request_text(http_method:, endpoint:, params: {})
+        client.headers['Accept'] = 'application/json'
+        client.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
+        response = client.public_send(http_method, endpoint, params)
+        response.body
+      end
+
+      def request_json(http_method:, endpoint:, params: {})
+        client.headers['Accept'] = 'application/json'
+        response = client.public_send(http_method, endpoint, params)
+        Oj.load(response.body)
+      end
+
+      def request_jsonld(http_method:, endpoint:, params: {})
+        client.headers['Accept'] = 'application/ld+json'
+        client.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
+        response = client.public_send(http_method, endpoint, params)
+        Oj.load(response.body)
+      end
+
+      def request_turtle(http_method:, endpoint:, params: {})
+        client.headers['Accept'] = 'text/turtle'
+        response = client.public_send(http_method, endpoint, params)
+        response.body
+      end
+    end
+  end
+end
---
+++ test/services/artsdata_api/v1/client_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+include ArtsdataAPI::V1
+
+class ClientTest < ActionView::TestCase
+
+  test "do execute_sparql" do
+    expected = "Dara Ó Briain"
+    client = ArtsdataAPI::V1::Client.new()
+    sparql = "PREFIX schema: <http://schema.org/> select ?name where { <http://kg.artsdata.ca/resource/K12-298> schema:name ?name }"
+    assert_equal expected, client.execute_sparql(sparql).first['name']['value']
+  end
+
+end
