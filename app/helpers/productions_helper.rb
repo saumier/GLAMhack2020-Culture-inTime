1609d13a95e7473df0ffdcfd6a0fbb377c08afdb
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-06-05 11:54:58 -0400

    added alternate image

--- app/helpers/productions_helper.rb
+++ app/helpers/productions_helper.rb
@@ -1,2 +1,13 @@
 module ProductionsHelper
+
+  require 'digest/md5'
+
+  def production_image(production)
+    if production.main_image.blank?
+      color = Digest::MD5.hexdigest(production.label)[0..5]
+      "https://dummyimage.com/300x300/#{color}/ffffff.png&text=#{production.label}"
+    else
+      production.main_image
+    end
+  end
 end
