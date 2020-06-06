0893f1a2fb22f3b90ac4a7242b93a48c39e1d8b0
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-06-06 06:34:46 -0400

    order updated_at: :desc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   # GET /
   def index
-    @most_viewed = Production.limit(5)
+    @most_viewed = Production.order(updated_at: :desc).limit(5)
   end
 
 end
