125cee34558e904dd4ea193d71971640a34f76bc
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-06-05 23:20:56 -0400

    added Country to search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
 
   def index
-    @productions = Production.where('lower(label) LIKE :search OR lower(locality) LIKE :search', search: "%#{params[:query].downcase}%")
+    @productions = Production.where('lower(label) LIKE :search OR lower(locality) LIKE :search OR lower(country) LIKE :search', search: "%#{params[:query].downcase}%")
   end
-
 end
