24d9988533e14cf359e4c4826747a3543f61689f
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-06-05 23:52:57 -0400

    removed log

--- app/services/artsdata_api/v1/client.rb
+++ app/services/artsdata_api/v1/client.rb
@@ -41,7 +41,6 @@ module ArtsdataAPI
         client.headers['Accept'] = 'application/json'
         client.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
         response = client.public_send(http_method, endpoint, params)
-        puts response.inspect
         Oj.load(response.body)
       end
 
