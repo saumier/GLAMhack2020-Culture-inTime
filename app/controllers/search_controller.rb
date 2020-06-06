e765321568931e9be9ef314ccdd7271ca512a9fb
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-06-06 07:58:47 -0400

    Added search by location_label

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
       date_range = [ DateTime.parse(params[:start])..DateTime.parse(params[:end])]
       @productions = Production.where(date_of_first_performance: date_range).order(:date_of_first_performance)
     elsif params[:query]
-      @productions = Production.where('lower(label) LIKE :search OR lower(locality) LIKE :search OR lower(country) LIKE :search', search: "%#{params[:query].downcase}%")
+      @productions = Production.where('lower(label) LIKE :search  OR lower(locality) LIKE :search OR lower(location_label) LIKE :search OR lower(country) LIKE :search', search: "%#{params[:query].downcase}%")
     else
       @productions = Production.all
     end
