3d53d811e490c7c65d1a0f5886715ae7a064ab4d
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-06-08 15:59:52 -0400

    Added timespan to spotlight query

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,11 @@ class SearchController < ApplicationController
   def index
     if params[:start]
       date_range = [ DateTime.parse(params[:start])..DateTime.parse(params[:end])]
-      @productions = Production.where(date_of_first_performance: date_range).order(:date_of_first_performance)
+      if params[:query]
+        @productions = Production.where(date_of_first_performance: date_range).order(:date_of_first_performance).where('lower(label) LIKE :search  OR lower(locality) LIKE :search OR lower(location_label) LIKE :search OR lower(country) LIKE :search', search: "%#{params[:query].downcase}%")
+      else
+        @productions = Production.where(date_of_first_performance: date_range).order(:date_of_first_performance)
+      end
     elsif params[:query]
       @productions = Production.where('lower(label) LIKE :search  OR lower(locality) LIKE :search OR lower(location_label) LIKE :search OR lower(country) LIKE :search', search: "%#{params[:query].downcase}%").order(:date_of_first_performance)
     else
