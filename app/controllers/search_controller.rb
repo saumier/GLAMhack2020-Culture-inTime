b93fca926e34ef561f3a5e0b1f39d8b32e776fb7
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-06-09 16:20:10 -0400

    code layout

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,16 +2,24 @@ class SearchController < ApplicationController
 
   def index
     if params[:start]
-      date_range = [ DateTime.parse(params[:start])..DateTime.parse(params[:end])]
+      date_range = [DateTime.parse(params[:start])..DateTime.parse(params[:end])]
       if params[:query]
-        @productions = Production.where(date_of_first_performance: date_range).order(:date_of_first_performance).where('lower(label) LIKE :search  OR lower(locality) LIKE :search OR lower(location_label) LIKE :search OR lower(country) LIKE :search', search: "%#{params[:query].downcase}%")
+        @productions = Production
+                       .where(date_of_first_performance: date_range)
+                       .where('lower(label) LIKE :search OR lower(locality) LIKE :search OR lower(location_label) LIKE :search OR lower(country) LIKE :search', search: "%#{params[:query].downcase}%")
+                       .order(:date_of_first_performance)
       else
-        @productions = Production.where(date_of_first_performance: date_range).order(:date_of_first_performance)
+        @productions = Production
+                       .where(date_of_first_performance: date_range)
+                       .order(:date_of_first_performance)
       end
     elsif params[:query]
-      @productions = Production.where('lower(label) LIKE :search  OR lower(locality) LIKE :search OR lower(location_label) LIKE :search OR lower(country) LIKE :search', search: "%#{params[:query].downcase}%").order(:date_of_first_performance)
+      @productions = Production
+                     .where('lower(label) LIKE :search OR lower(locality) LIKE :search OR lower(location_label) LIKE :search OR lower(country) LIKE :search', search: "%#{params[:query].downcase}%")
     else
-      @productions = Production.all
+      @productions = Production
+                     .order(:date_of_first_performance)
+                     .limit(1000)
     end
   end
 end
