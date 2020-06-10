f24de7181248a14a12dc322fb15eaa92f632eaad
Gregory Saumier-Finch <gregory@culturecreates.com>
2020-06-10 15:38:26 -0400

    added json api

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,9 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import ReactOnRails from 'react-on-rails';
+import Search from '../components/Search';
+
+ReactOnRails.register({
+  Search
+});
