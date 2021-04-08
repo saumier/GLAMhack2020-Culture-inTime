69562543912b74062b0ec0a0d38eb0a05dd3a80f
Gregory Saumier-Finch <gregory@culturecreates.com>
2021-04-08 15:56:17 -0400

    Added refresh buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-
   root 'home#index'
   get '/search', to: 'search#index'
 
   get '/spotlight', to: 'home#spotlight'
 
-  get '/admin', to: 'home#admin'
+  get '/admin', to: 'admin#index', as: 'admin'
+
+  get '/admin/build_cache', to: 'admin#build_cache', as: 'admin_build_cache'
+ 
 
   resources :productions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
